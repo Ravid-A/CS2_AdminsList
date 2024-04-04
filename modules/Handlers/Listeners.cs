5a72bd3addc7ac724abb05181d661c560826225b
Ravid-A <ravid2903@gmail.com>
2024-04-04 12:06:49 +0300

    Added listener to remove player on disconnect and clear list on map end

--- modules/Handlers/Listeners.cs
+++ modules/Handlers/Listeners.cs
@@ -10,8 +10,15 @@ public static class Listeners
     public static void RegisterListeners()
     {
         CS2_AdminsList Plugin = CS2_AdminsList.Instance;
+        Plugin.RegisterListener<CounterStrikeSharp.API.Core.Listeners.OnMapEnd>(OnMapEnd);
         Plugin.RegisterListener<CounterStrikeSharp.API.Core.Listeners.OnClientAuthorized>(OnClientAuthorized);
+        Plugin.RegisterListener<CounterStrikeSharp.API.Core.Listeners.OnClientDisconnect>(OnClientDisconnect);
     }  
+
+    public static void OnMapEnd()
+    {
+        CS2_AdminsList.Players.Clear();
+    }
 
     public static void OnClientAuthorized(int playerSlot, SteamID steamID)
     {
@@ -26,5 +33,10 @@ public static class Listeners
 
         string query = string.Format("SELECT `name`, `show_in_list` FROM `admin_list_settings` WHERE `auth` = '{0}'", playerSteamID.SteamId64.ToString());
         Database.Query(Database.SQL_FetchUser_CB, query, playerSlot);
+    }
+
+    public static void OnClientDisconnect(int playerSlot)
+    {
+        CS2_AdminsList.Players.RemoveAt(CS2_AdminsList.Players.FindIndex(player => player.Player.Slot == playerSlot));
     }
 }
